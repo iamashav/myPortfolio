093d1654ee3a8121e0a7575f44b3aa41eb91c2bb
Ashav Parihar <ashavparihar7@gmail.com>
2026-08-02 21:09:20 +1200

    Replace native scrollbar with custom cyan-on-black overlay

--- apps/site/src/App.tsx
+++ apps/site/src/App.tsx
@@ -4,6 +4,7 @@ import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Hero } from './components/Hero/Hero';
 import { Projects } from './components/Projects/Projects';
+import { Scrollbar } from './components/Scrollbar/Scrollbar';
 import { TechStrip } from './components/TechStrip/TechStrip';
 import { useReveal } from './hooks/useReveal';
 import { useSmoothScroll } from './hooks/useSmoothScroll';
@@ -23,6 +24,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <Scrollbar />
     </>
   );
 }
